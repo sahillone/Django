085ab6a69cb4ea6cacd1f066c8d085cb861b1cd2
sahillone <sahillone17222@gmail.com>
2022-11-17 22:15:57 +0530

    first commit

---
+++ blog/urls.py
@@ -0,0 +1,26 @@
+from django.urls import path
+from .views import (
+    PostListView,
+    PostDetailView,
+    PostCreateView,
+    PostUpdateView,
+    PostDeleteView,
+    UserPostListView,
+)
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path("", PostListView.as_view(), name="blog_home"),
+    path("post/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
+    path("post/new/", PostCreateView.as_view(), name="post-create"),
+    path("post/<int:pk>/update/", PostUpdateView.as_view(), name="post-update"),
+    path("post/<int:pk>/delete/", PostDeleteView.as_view(), name="post-delete"),
+    path("user/<str:username>/", UserPostListView.as_view(), name="user-posts"),
+    path("about/", views.about, name="about_page"),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
